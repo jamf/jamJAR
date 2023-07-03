ddb247b5035d2af8080fefa9519e5d06372b9645
macmule <2464974+macmule@users.noreply.github.com>
2023-07-03 16:08:11 +0100

    Update ea-jamJAR.sh

--- extension_attribute/ea-jamJAR.sh
+++ extension_attribute/ea-jamJAR.sh
@@ -2,7 +2,7 @@
 
 ####################################################################################################
 #
-# Copyright (c) 2021, dataJAR Ltd.  All rights reserved.
+# Copyright (c) 2023, dataJAR Ltd.  All rights reserved.
 #
 #       Redistribution and use in source and binary forms, with or without
 #       modification, are permitted provided that the following conditions are met:
